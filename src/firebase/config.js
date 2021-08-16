fa7d69603c9178447d9397680d05ab99a2c398a7
Rich Hildred <rhildred@gmail.com>
2021-08-16 15:12:01 -0400

    remove my credentials

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,23 +3,10 @@ import '@firebase/auth';
 import '@firebase/database';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAfGYryRXI52Qs4jNCfYo69LS2L33tzFL4",
-
-    authDomain: "prog8110summer2021.firebaseapp.com",
-
-    databaseURL: "https://prog8110summer2021-default-rtdb.firebaseio.com",
-
-    projectId: "prog8110summer2021",
-
-    storageBucket: "prog8110summer2021.appspot.com",
-
-    messagingSenderId: "871503641834",
-
-    appId: "1:871503641834:web:a1f58dd2e03dbdefec6b93"
-
+    apiKey: "Copy Your config from the firebase console"
 };
 
 if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig);
-}
+} 
 export { firebase };
