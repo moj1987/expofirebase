8a580d4457a9d2c5f6b3f8bb67eb3d27397927f4
Mojtaba Abbasszadeh <mojtaba.abbasszadeh@gmail.com>
2021-08-20 16:43:39 -0400

    Add Firebase credentials to src/config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,7 +3,22 @@ import '@firebase/auth';
 import '@firebase/database';
 
 const firebaseConfig = {
-    apiKey: "Copy Your config from the firebase console"
+  apiKey: "AIzaSyBjNaMNp4WhU4Sknc11wHekcndhNF6wtrQ",
+
+  authDomain: "project-5479518509391935408.firebaseapp.com",
+
+  databaseURL:
+    "https://project-5479518509391935408-default-rtdb.firebaseio.com",
+
+  projectId: "project-5479518509391935408",
+
+  storageBucket: "project-5479518509391935408.appspot.com",
+
+  messagingSenderId: "373335282610",
+
+  appId: "1:373335282610:web:312b0407bd2cbde7a94aab",
+
+  measurementId: "G-L423NJML1C",
 };
 
 if (!firebase.apps.length) {
